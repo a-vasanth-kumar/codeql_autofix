3d05239d3b62d268cb451497a53d47c4eb258d04
a-vasanth-kumar <55576815+a-vasanth-kumar@users.noreply.github.com>
2025-01-20 10:56:48 +0530

    Update CookieExample.java

--- src/main/java/com/javaexamples/codeql/CookieExample.java
+++ src/main/java/com/javaexamples/codeql/CookieExample.java
@@ -1,5 +1,6 @@
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
 
 public class CookieExample {
 
@@ -7,6 +8,16 @@ public class CookieExample {
         Cookie cookie = new Cookie("sessionId", "123456");
         cookie.setHttpOnly(true); // GOOD: HttpOnly flag set
         cookie.setSecure(false); // BAD: Secure flag not set
+
+        Random r = new Random();
+
+        byte[] bytes = new byte[16];
+        r.nextBytes(bytes);
+        
+        String cookieValue = encode(bytes);
+        
+        Cookie cookie = new Cookie("name", cookieValue);
+        response.addCookie(cookie);
         response.addCookie(cookie);
     }
 }
