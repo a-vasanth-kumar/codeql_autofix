dcf6091b5fd1372821efc94ce792bba94acda3ab
a-vasanth-kumar <55576815+a-vasanth-kumar@users.noreply.github.com>
2025-01-20 10:34:57 +0530

    Update HomeController.java

--- src/main/java/com/javaexamples/codeql/HomeController.java
+++ src/main/java/com/javaexamples/codeql/HomeController.java
@@ -2,12 +2,21 @@ package com.javaexamples.codeql;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
-        return "Hello, World!";
+    public String home(@RequestParam(value = "username", defaultValue = "name") String username) {
+        log.warn("User:'{}'", username);
+        // The logging call above would result in multiple log entries as shown below:
+        // User:'Guest'
+        // User:'Admin'
+        return username;
     }
 }
